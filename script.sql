5940437972a4e1546bd8778d86e413b44e140041
NEON <wafigrissi@gmail.com>
2017-12-15 10:32:08 +0100

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,64 @@
+DROP TABLE IF EXISTS Utilisateur CASCADE;
+DROP TABLE IF EXISTS Login CASCADE;
+
+CREATE TABLE Login
+(
+    idLogin int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    mail text CHECK( mail='%@%.%' ),
+    mdp text CHECK( length(mdp)>=4 ),
+    type varchar(30) NOT NULL
+);
+
+CREATE TABLE Utilisateur
+(
+    idUser int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idLogin int NOT NULL,
+    nom varchar(50) NOT NULL,
+    prenom varchar(50) NOT NULL,
+    note int CHECK(note>=0 AND note<=5),
+    argent double UNSIGNED,
+    FOREIGN KEY (idLogin) REFERENCES Login(idLogin),
+    UNIQUE(nom,prenom)
+);
+
+CREATE TABLE Service
+(
+    idService int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idUser int NOT NULL,
+    FOREIGN KEY (idUser) REFERENCES Utilisateur(idUser)
+);
+
+CREATE TABLE Trajet
+(
+    idTrajet int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idService int NOT NULL,
+    idUser int NOT NULL,
+    villeD varchar(30) NOT NULL,
+    villeA varchar(30) NOT NULL,
+    heureD date NOT NULL,
+    heureA date NOT NULL,
+    tarif double UNSIGNED,
+    FOREIGN KEY (idService) REFERENCES Service(idService),
+    FOREIGN KEY (idUser) REFERENCES Utilisateur(idUser)
+)
+
+CREATE TABLE Location
+(
+    idLocation int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idService int NOT NULL,
+    idUser int NOT NULL,
+    dateD date NOT NULL,
+    dateF date NOT NULL,
+    adresse text NOT NULL,
+    tarif double UNSIGNED,
+    FOREIGN KEY (idService) REFERENCES Service(idService),
+    FOREIGN KEY (idUser) REFERENCES Utilisateur(idUser)
+)
+
+CREATE TABLE Reservation
+(
+    idReservation int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idService int NOT NULL,
+    dateReserv date NOT NULL,
+)
+
