0dd2b7496ce8c59b91252391984c07e2e7c78a0a
NEON <wafigrissi@gmail.com>
2017-12-15 11:49:55 +0100

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,9 +1,15 @@
-DROP TABLE IF EXISTS Service CASCADE;
-DROP TABLE IF EXISTS Utilisateur CASCADE;
-DROP TABLE IF EXISTS Login CASCADE;
 DROP TABLE IF EXISTS Trajet CASCADE;
 DROP TABLE IF EXISTS Location CASCADE;
 DROP TABLE IF EXISTS Reservation CASCADE;
+DROP TABLE IF EXISTS Service CASCADE;
+DROP TABLE IF EXISTS Velo CASCADE;
+DROP TABLE IF EXISTS Voiture CASCADE;
+DROP TABLE IF EXISTS Vehicule CASCADE;
+DROP TABLE IF EXISTS Local CASCADE;
+DROP TABLE IF EXISTS Ressource CASCADE;
+DROP TABLE IF EXISTS Messagerie CASCADE;
+DROP TABLE IF EXISTS Utilisateur CASCADE;
+DROP TABLE IF EXISTS Login CASCADE;
 
 CREATE TABLE Login
 (
@@ -64,7 +70,61 @@ CREATE TABLE Reservation
     idReservation int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     idService int NOT NULL,
     dateReserv date NOT NULL,
-    comm double NOT NULL,
     FOREIGN KEY (idService) REFERENCES Service(idService)
 );
 
+CREATE TABLE Recherche
+(
+    idRecherche int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idUser int NOT NULL,
+    FOREIGN KEY (idUser) REFERENCES Utilisateur(idUser)
+);
+
+CREATE TABLE Messagerie
+(
+    idMessage int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idUser1 int NOT NULL,
+    idUser2 int NOT NULL,
+    msg text NOT NULL,
+    FOREIGN KEY (idUser1) REFERENCES Utilisateur(idUser),
+    FOREIGN KEY (idUser2) REFERENCES Utilisateur(idUser)
+);
+
+CREATE TABLE Ressource
+(
+    idRessource int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idService int NOT NULL,
+    type varchar(30) NOT NULL,
+    FOREIGN KEY (idService) REFERENCES Service(idService)
+);
+
+CREATE TABLE Local
+(
+    idLocal int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idRessource int NOT NULL,
+    FOREIGN KEY (idRessource) REFERENCES Ressource(idRessource)
+);
+
+CREATE TABLE Vehicule
+(
+    idVehicule int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idRessource int NOT NULL,
+    FOREIGN KEY (idRessource) REFERENCES Ressource(idRessource)
+);
+
+CREATE TABLE Voiture
+(
+    idVoiture int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    idVehicule int NOT NULL,
+    nbPlaces int UNSIGNED NOT NULL,
+    nbBagages int UNSIGNED,
+    FOREIGN KEY (idVehicule) REFERENCES Vehicule(idVehicule)
+);
+
+CREATE TABLE Velo
+(
+    idVelo int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    desription text,
+    idVehicule int NOT NULL,
+    FOREIGN KEY (idVehicule) REFERENCES Vehicule(idVehicule)
+);
