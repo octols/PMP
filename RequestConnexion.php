b0ab4ef602c98983b1988ff0f15d958173a71b4e
Griff <victorwa@hotmail.fr>
2017-12-18 09:48:39 +0100

    Update RequestConnexion.php

--- RequestConnexion.php
+++ RequestConnexion.php
@@ -2,49 +2,49 @@
 /**
  * Created by PhpStorm.
  * User: Moi
- * Date: 15/12/2017
- * Time: 10:27
+ * Date: 18/12/2017
+ * Time: 09:47
  */
     //connect on ?ySQL
-    $con = mysqli_connect('localhost','root','') or die(mysqli_error());
-    if (!$con) {
-        error_log("Unable to connect to DB: " . mysqli_error());
-        exit;
+    if (isset($_POST['connexion']) AND isset($_POST['pseudo']) AND isset($_POST['mdp'])) {
+        try {
+            $bdd = new PDO('mysql:host=localhost;dbname=projetphp;charset=utf8', 'root', '');
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        $qry = "SELECT * FROM login WHERE mail='" . $_POST['pseudo'] . "' AND mdp='" . $_POST['mdp'] . "'";
+        $res = $bdd->query($qry);
+        $donnees=$res->fetch();
+
+    if($donnees!=null) {
+            //if ($num_row == 1) {
+            session_start();
+            $_SESSION['mail'] = $donnees['mail'];
+            $_SESSION['idlog'] = $donnees['idLogin'];
+            $_SESSION['typelog'] = $donnees['type'];
+         }
+        else{
+            header('Location: index.php');
+            exit();
+        }
+        $res->closeCursor();
+        if($donnees!=null) {
+            $query = "SELECT * FROM utilisateur WHERE idLogin=" . $_SESSION['idlog'] . " ";
+
+            $reponse = $bdd->query($query);
+            $donnees=$reponse->fetch();
+             $_SESSION['idUser'] = $donnees['idUser'];
+            $_SESSION['uPrenom'] = $donnees['prenom'];
+            $_SESSION['uNom'] = $donnees['nom'];
+            $_SESSION['note']= $donnees['note'];
+            $_SESSION['argent']=$donnees['argent'];
+            header('Location: ../View/Profil.php');
+            exit();
+        } else {
+            header('Location: index.php');
+            exit();
+        }
+        $reponse->closeCursor();
     }
-
-    if (!$con) {
-        error_log("Unable to connect to DB: " . mysqli_error());
-        exit;
-    }
-
-    if (!mysqli_select_db($con, "projetphp")) {
-        error_log("Unable to select mydbname: " . mysqli_error());
-        exit;
-    }
-      $uName = $_POST['username'];
-      $pWord = $_POST['password'];
-
-      $qry = "SELECT * FROM login WHERE mail='".$uName."' AND mdp='".$pWord."'";
-
-      $res = mysqli_query($con, $qry);
-      $num_row = mysqli_num_rows($res);
-      //error_log($num_row);
-      $row=mysqli_fetch_assoc($res);
-      if( $num_row == 1 ) {
-          session_start();
-          $_SESSION['uName'] = $row['username'];
-          $_SESSION['idlog'] = $row['idLogin'];
-          $_SESSION['typelog'] = $row['type'];
-
-          $query = "SELECT idUser FROM utilisateur WHERE idLogin=".$_SESSION['idlog']." ";
-
-          $con->query($query);
-          $res = mysqli_query($con, $query);
-          $row = mysqli_fetch_array($res);
-          $_SESSION['idUser'] = $row[0];
-          echo 'true';
-      }
-      else {
-          echo 'false';
-      }
 ?>
