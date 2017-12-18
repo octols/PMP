0e2b3b4f4ab030fe66f1cbfaaf58bad4b908d832
Griff <victorwa@hotmail.fr>
2017-12-18 11:59:37 +0100

    Add files via upload

---
+++ RequestConnexion.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Moi
+ * Date: 18/12/2017
+ * Time: 09:47
+ */
+    /*$con = mysqli_connect('localhost','root','') or die(mysqli_error());
+    if (!$con) {
+        error_log("Unable to connect to DB: " . mysqli_error());
+        exit;
+    }
+
+    if (!$con) {
+        error_log("Unable to connect to DB: " . mysqli_error());
+        exit;
+    }
+
+    if (!mysqli_select_db($con, "projetphp")) {
+        error_log("Unable to select mydbname: " . mysqli_error());
+        exit;
+    }*/
+    if (isset($_POST['connexion']) AND isset($_POST['pseudo']) AND isset($_POST['mdp'])) {
+        try {
+            $bdd = new PDO('mysql:host=localhost;dbname=pmp;charset=utf8', 'root', '');
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        $qry = "SELECT * FROM login WHERE mail='" . $_POST['pseudo'] . "' AND mdp='" . $_POST['mdp'] . "'";
+        $res = $bdd->query($qry);
+        $donnees=$res->fetch();
+        /*$res = mysqli_query($bdd, $qry);
+        $num_row = mysqli_num_rows($res);
+        //error_log($num_row);
+        $row = mysqli_fetch_assoc($res);*/
+        if($donnees!=null) {
+            //if ($num_row == 1) {
+            session_start();
+            $_SESSION['mail'] = $donnees['mail'];
+            $_SESSION['idlog'] = $donnees['idLogin'];
+            $_SESSION['typelog'] = $donnees['type'];
+            /*$_SESSION['mail'] = $row['mail'];
+            $_SESSION['idlog'] = $row['idLogin'];
+            $_SESSION['typelog'] = $row['type'];*/
+        }
+        else{
+            header('Location: ../index.php');
+            exit();
+        }
+        $res->closeCursor();
+
+        if($_POST['pseudo']=="root"){
+            header('Location: ../View/Profil.php');
+            exit();
+        }
+        else{
+            $query = "SELECT * FROM utilisateur WHERE idLogin=" . $_SESSION['idlog'] . " ";
+
+            $reponse = $bdd->query($query);
+            $donnees=$reponse->fetch();
+            /*$res = mysqli_query($bdd, $query);
+            $num_row = mysqli_num_rows($res);
+            //error_log($num_row);
+            $row = mysqli_fetch_assoc($res);*/
+            if($donnees!=null) {
+                $_SESSION['idUser'] = $donnees['idUser'];
+                $_SESSION['uPrenom'] = $donnees['prenom'];
+                $_SESSION['uNom'] = $donnees['nom'];
+                $_SESSION['note']= $donnees['note'];
+                $_SESSION['argent']=$donnees['argent'];
+                /*$_SESSION['idUser'] = $row['idUser'];
+                $_SESSION['uPrenom'] = $row['prenom'];
+                $_SESSION['uNom'] = $row['nom'];*/
+
+                header('Location: ../View/Profil.php');
+                exit();
+            } else {
+                header('Location: ../index.php');
+                exit();
+            }
+            $reponse->closeCursor();
+        }
+    }
+
+?>
---
+++ messagerie2.php
@@ -0,0 +1,13 @@
+<?php
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=pmp;charset=utf8','root','');
+}
+catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+$idExp = $bdd->prepare('SELECT INTO login(idLogin) where mail= exp');
+$idDes = $bdd->prepare('SELECT INTO utilisateur(idLogin) where mail= exp');
+$req = $bdd->prepare('INSERT INTO messagerie(idMessage, idUser1, idUser2, msg ) 
+	VALUES(?,?,?,?)');
+$req->execute(array($_POST['idExp'],$_POST['idDes'],$_POST['msg']));
+?>
