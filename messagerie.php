601a2bcc7c38136a173f940f57d500c7983da47e
AlexLefebvre <message.alefe@gmail.com>
2017-12-18 08:17:17 +0100

    Add files via upload

---
+++ messagerie.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head><title>Messagerie</title>
+  <h2>Bienvenue dans la messagerie</h2>
+  <br>
+
+
+    <form action="messagerie2.php" method="post">
+    <p>
+      <p><strong>Votre adresse Mail: </strong>
+        <input type="text" name="exp" /></p>
+
+      <p><strong>Adresse Mail destinataire: </strong>
+        <input type="text" name="destinataire" /></p>
+
+     <p><strong>Message :</strong><br>
+        <textarea type="text" name="msg" cols="50" rows="10" ;  style="height: 200px; width: 300px;">  </textarea></p>
+
+  <br>  <br>  <br>
+    <input type="submit" value="Envoyer" />
+
+    </p>
+</form>
